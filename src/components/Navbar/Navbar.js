1f570804709a7698dddae999de621a201fd3082c
maxwell-paulo <maxpaulo.mps@gmail.com>
2022-11-04 20:22:48 -0300

    instalations and files creation

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,3 @@
+export function Navbar() {
+  return <h1 className="font-Roboto">Navbar</h1>;
+}
---
+++ src/pages/Home/index.js
@@ -0,0 +1,10 @@
+import { Modal } from "../../components/Modal/Modal.js";
+
+export function Home() {
+  return (
+    <>
+      <Modal />
+      <h1>Aqui é a Home</h1>
+    </>
+  );
+}
