cf5f6ccea98e68698a11c9029ad9fb8068d338f4
maxwell-paulo <maxpaulo.mps@gmail.com>
2022-11-04 21:23:40 -0300

    Modal cards creation and modal cards home page rendering wip

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,3 +1,12 @@
-export function Modal() {
-  return <h1>Modal</h1>;
+export function Modal(props) {
+  const { imageUrl, title, publishedAt, summary, url } = props;
+  return (
+    <div>
+      <img src={imageUrl} alt="Article" />
+      <h1>{title}</h1>
+      <p>{publishedAt}</p>
+      <p>{summary}</p>
+      <a href={url}>Ir para o site</a>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    collors: {
+      dakGray: "#1E2022",
+      darkPurple: "#302E53",
+      siteOrange: "#D07017",
+    },
     extend: {
       fontFamily: {
         Roboto: ["Roboto Condensed", "sans-serif"],
