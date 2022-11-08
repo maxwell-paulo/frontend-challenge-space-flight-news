5303dcf20dd41523dd800904c3858be0f3e9d968
maxwell-paulo <maxpaulo.mps@gmail.com>
2022-11-08 15:13:07 -0300

    readme

--- src/components/HomePageCard/HomePageCard.js
+++ src/components/HomePageCard/HomePageCard.js
@@ -4,7 +4,7 @@ export function HomePageCard(props) {
   const { imageUrl, title, publishedAt, summary, url, id, index } = props;
   if (index % 2 === 1) {
     return (
-      <div className="flex m-auto mb-14 justify-between items-center w-4/6   gap-x-20">
+      <div className="flex m-auto mb-20 justify-between items-center w-4/6   gap-x-20">
         <img src={imageUrl} alt="Article" className="max-w-md w-60" />
         <div className="flex flex-col gap-1.5">
           <h1 className="font-Roboto font-black text-[#1E2022]">{title}</h1>
@@ -29,7 +29,7 @@ export function HomePageCard(props) {
     );
   } else {
     return (
-      <div className="flex m-auto mb-14 justify-between items-center w-4/6   gap-x-20">
+      <div className="flex m-auto mb-20 justify-between items-center w-4/6   gap-x-20 ">
         <div className="flex flex-col gap-1.5">
           <h1 className="font-Roboto font-black text-[#1E2022]">{title}</h1>
           <div className="flex justify-between items-center">
