dde762ff0b66b539a413510d90bc22b8987a7111
maxwell-paulo <maxpaulo.mps@gmail.com>
2022-11-08 14:01:15 -0300

    navbar styled

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,11 @@
+import logo from "../../images/space-flight-news-logo.png";
+
 export function Navbar() {
   return (
-    <div>
-      <h1>imagem</h1>
-      <h1>Space Flight News</h1>
+    <div className="flex flex-col justify-center - items-center mt-7 mb-10 gap-3">
+      <img src={logo} alt="Space flight news logo" />
+      <p className="font-Roboto text-[#1E2022]">Space Flight News</p>
+      <div className="w-11/12 h-0.5 bg-gray-300"></div>
     </div>
   );
 }
